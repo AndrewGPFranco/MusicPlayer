789e96d7f48b90348ceb9a9da02d77022e260d5a
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2024-01-08 16:35:34 +0000

    Lógica para o usuário conseguir saber a rota que está pelo menu

--- src/components/Global/Menu.jsx
+++ src/components/Global/Menu.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import style from "./Menu.module.css";
 import iconHome from "../../assets/home.png";
 import iconAll from "../../assets/all.png";
@@ -6,22 +6,24 @@ import iconGames from "../../assets/games.png";
 import iconCommon from "../../assets/music.png";
 
 const Menu = () => {
+  const location = useLocation();
+
   return (
     <div className={style.menu}>
       <ul>
-        <li>
+        <li className={location.pathname === '/' ? style.active : ''}>
           <img src={iconHome} alt="Logo de uma Casa Residencial" />
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/songs' ? style.active : ''}>
           <img src={iconAll} alt="Logo de uma Casa Residencial" />
           <Link to="/songs">Songs</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/songs/games' ? style.active : ''}>
           <img src={iconGames} alt="Logo de uma Casa Residencial" />
           <Link to="/songs/games">Gamer</Link>
         </li>
-        <li>
+        <li className={location.pathname === '/songs/common' ? style.active : ''}>
           <img src={iconCommon} alt="Logo de uma Casa Residencial" />
           <Link to="/songs/common">Common</Link>
         </li>
