10f29226e406d1d9d51034d2d185a23ab54427c0
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2024-01-08 17:46:22 +0000

    Componente de carrossel de imagens e bootstrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Menu from '../components/Global/Menu.jsx';
+import Carousel from '../components/Home/Carousel.jsx';
 
 const Home = () => {
   return (
     <div>
+      <Carousel />
       <Menu />
     </div>
   )
