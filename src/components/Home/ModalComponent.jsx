4a3876f49eda0728b93baa14896a188a4208f673
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2024-01-09 08:44:10 +0000

    Bootstrap react instalado e componente de modal

---
+++ src/components/Home/ModalComponent.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { Link } from 'react-router-dom';
+import perfil from "../../assets/perfil.jpg";
+import style from "./ModalComponent.module.css";
+
+function ModalComponent() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className={style.modal}>
+      <Button id="btn" onClick={handleShow}>
+        <img src={perfil} alt="Imagem de Perfil do Usuário" />
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Link to="perfil">Access Perfil</Link>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default ModalComponent;
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -1,26 +1,12 @@
-import dot from "../../assets/dot.svg";
-import perfil from "../../assets/perfil.jpg";
 import style from "./Navbar.module.css";
+import ModalComponent from "./ModalComponent.jsx"
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className={style.navbar}>
-        <div className={style.perfil}>
-          <Link to="/perfil">
-            <img src={perfil} alt="Imagem de Perfil do Usuário" />
-          </Link>
-        </div>
-        <div className={style.about}>
-          <Link to="/about">
-            <img
-              className={style.dot}
-              src={dot}
-              alt="Icone de três pontinhos"
-            />
-          </Link>
-        </div>
+        <ModalComponent/>
       </nav>
     </>
   );
