aa74db5f705a3c01c71520517b06b89e62eb3e1d
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2024-01-09 01:11:58 +0000

    component de music

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,10 +1,26 @@
 import Menu from "../components/Global/Menu.jsx";
+import { useState } from "react";
 
 const Music = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const music = new Audio("../../public/sounds/couting.mp3");
+
+  let playMusic = () => {
+    music.play();
+    setIsPlaying(true);
+  };
+
+  let pauseMusic = () => {
+    music.pause();
+    setIsPlaying(false);
+  };
+  
   return (
     <div>
       <Menu />
       <h1>Welcome to Music!</h1>
+      <button onClick={playMusic}>Play</button>
+      <button onClick={pauseMusic}>Pause</button>
     </div>
   );
 };
