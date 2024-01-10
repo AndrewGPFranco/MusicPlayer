68759af5f0ebd247c1c313dfddf97acb29b1c832
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2024-01-10 11:58:58 +0000

    Teste

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,20 +1,30 @@
 import Menu from "../components/Global/Menu.jsx";
 import play from "../assets/play.png";
+import { useState } from "react";
 
 const Music = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
   const music = new Audio("/sounds/couting.mp3");
 
   const playMusic = () => {
     music.play();
+    setIsPlaying(true);
   };
+
+  const pauseMusic = () => {
+    music.pause();
+    setIsPlaying(false);
+  }
   
   return (
     <div className="containerMusic">
       <Menu />
-      <h1>Press to listen to your music!</h1>
+      <h1>Press to listen</h1>
+      <h1>to your music!</h1>
       <button className="btnPlay2" onClick={playMusic}>
         <img className="btnPlay" src={play} alt="Icone de Play" />
       </button>
+      <button className="btnPause2" onClick={pauseMusic}>Pause</button>
     </div>
   );
 };
