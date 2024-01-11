23db7ff0ad9f6e1ed54886ead3e8bf670cb4bae4
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-10 21:37:16 -0300

    componente v1

--- src/components/Home/Moment.jsx
+++ src/components/Home/Moment.jsx
@@ -2,41 +2,41 @@ import { useState, useEffect } from "react";
 import style from "./Moment.module.css";
 
 function Moment() {
-    const [jsonData, setJsonData] = useState(null);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("/musicMoment.json");
-                const data = await response.json();
-                setJsonData(data);
-            } catch (error) {
-                console.error("Erro ao carregar dados JSON", error);
-            }
-        };
+  const [jsonData, setJsonData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/musicMoment.json");
+        const data = await response.json();
+        setJsonData(data);
+      } catch (error) {
+        console.error("Erro ao carregar dados JSON", error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div className={style.music}>
-            {jsonData && (
-                <ul>
-                    {jsonData.map((item, index) => (
-                        <li key={index}>
-                            <img
-                                src={item.image}
-                                alt={`Imagem da categoria ${item.category}`}
-                            />
-                            <div className={style.info}>
-                                <h3>{item.singer}</h3>
-                                <p>{item.music}</p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    )
+  return (
+    <div className={style.music}>
+      {jsonData && (
+        <ul>
+          {jsonData.map((item, index) => (
+            <li key={index}>
+              <img
+                src={item.image}
+                alt={`Imagem da categoria ${item.category}`}
+              />
+              <div className={style.info}>
+                <h3>{item.singer}</h3>
+                <p>{item.music}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Moment;
