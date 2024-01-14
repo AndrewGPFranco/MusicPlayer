3920d63a15faf4882dd8b34af2915ea70ba8ffa9
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-13 21:07:45 -0300

    mudanças nas paginas

--- src/components/Global/Menu.jsx
+++ src/components/Global/Menu.jsx
@@ -5,31 +5,35 @@ import iconAll from "../../assets/all.png";
 import iconGames from "../../assets/games.png";
 import iconCommon from "../../assets/music.png";
 
-function Menu () {
+function Menu() {
   const location = useLocation();
 
   return (
     <div className={style.menu}>
       <ul>
-        <li className={location.pathname === '/' ? style.active : ''}>
+        <li className={location.pathname === "/" ? style.active : ""}>
           <img src={iconHome} alt="Logo de uma Casa Residencial" />
           <Link to="/">Home</Link>
         </li>
-        <li className={location.pathname === '/songs' ? style.active : ''}>
+        <li
+          className={location.pathname === "/songs/common" ? style.active : ""}
+        >
+          <img src={iconCommon} alt="Logo de uma Casa Residencial" />
+          <Link to="/songs/common">Common</Link>
+        </li>
+        <li className={location.pathname === "/songs" ? style.active : ""}>
           <img src={iconAll} alt="Logo de uma Casa Residencial" />
           <Link to="/songs">Songs</Link>
         </li>
-        <li className={location.pathname === '/songs/games' ? style.active : ''}>
+        <li
+          className={location.pathname === "/about" ? style.active : ""}
+        >
           <img src={iconGames} alt="Logo de uma Casa Residencial" />
-          <Link to="/songs/games">Gamer</Link>
-        </li>
-        <li className={location.pathname === '/songs/common' ? style.active : ''}>
-          <img src={iconCommon} alt="Logo de uma Casa Residencial" />
-          <Link to="/songs/common">Common</Link>
+          <Link to="/about">About</Link>
         </li>
       </ul>
     </div>
   );
-};
+}
 
 export default Menu;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,5 @@
 import Home from "../pages/Home.jsx";
 import Music from "../pages/Music.jsx";
-import Games from "../pages/Games.jsx";
 import Common from "../pages/Common.jsx";
 import Perfil from "../pages/Perfil.jsx";
 import About from "../pages/About.jsx";
@@ -13,7 +12,6 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/songs" element={<Music />}></Route>
-        <Route path="/songs/games" element={<Games />}></Route>
         <Route path="/songs/common" element={<Common />}></Route>
         <Route path="/perfil" element={<Perfil />}></Route>
         <Route path="/about" element={<About />}></Route>
