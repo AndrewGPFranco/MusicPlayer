94ab2a1d5f628a76ed92586533dd7419230a2935
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-14 12:48:43 -0300

    renderização e estilos feitos

--- src/components/Common/Music.jsx
+++ src/components/Common/Music.jsx
@@ -3,21 +3,52 @@ import tauz from "../../assets/tauz.jpg";
 import dot from "../../assets/dot.svg";
 
 function Music() {
+  const sounds = [
+    {
+      id: 1,
+      title: "Perfect",
+      artist: "Ed Sheeran",
+      image:
+        "https://cdns-images.dzcdn.net/images/cover/007b5e41dae0dd99ade00f509db734d4/350x350.jpg",
+      category: "Pop",
+    },
+    {
+      id: 2,
+      title: "Photograph",
+      artist: "Ed Sheeran",
+      image: "https://i1.sndcdn.com/artworks-000300249612-tt0oxz-t500x500.jpg",
+      category: "Pop",
+    },
+  ];
+
   return (
-    <div className={style.container}>
-      <div className={style.containerInfos}>
-          <div className={style.infosImagem}>
-            <img src={tauz} alt="Imagem" />
+    <section>
+      {sounds.map((sound) => {
+        return (
+          <div className={style.container}>
+            <div className={style.containerInfos}>
+              <div className={style.infosImagem}>
+                <img src={sound.image} alt="Imagem" />
+              </div>
+              <div className={style.infosTitle}>
+                <div>
+                  <h3>{sound.title}</h3>
+                </div>
+                <div className={style.categoryArtist}>
+                  <p>
+                    <scan className={style.scan}>{sound.category}</scan>
+                  </p>
+                  <p>{sound.artist}</p>
+                </div>
+              </div>
+            </div>
+            <div className={style.imagem}>
+              <img src={dot} alt="Imagem" />
+            </div>
           </div>
-          <div className={style.infosTitle}>
-            <h3>Perfect</h3>
-            <p>Ed Sheeran</p>
-          </div>
-      </div>
-      <div className={style.imagem}>
-        <img src={dot} alt="Imagem" />
-      </div>
-    </div>
+        );
+      })}
+    </section>
   );
 }
 
