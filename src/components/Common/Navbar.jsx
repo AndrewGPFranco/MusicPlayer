142edbb85a275dd20011852ac3736df492228272
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-14 14:49:23 -0300

    função para deixar dinamico e atualizado o numero de musicas que a lista contem

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,14 +1,17 @@
 import perfil from "../../assets/perfil.jpg";
 import style from "./Navbar.module.css";
+import sounds from "../../../public/sounds.json";
 
 function Navbar() {
+    const totalSounds = sounds.length;
+
     return (
         <nav className={style.container}>
             <img src={perfil} alt="Foto de Perfil" />
             <div className={style.infos}>
                 <ul>
                     <li><strong>@dev_gps</strong></li>
-                    <li>50 Sounds</li>
+                    <li>{ totalSounds } Sounds</li>
                 </ul>
             </div>
         </nav>
