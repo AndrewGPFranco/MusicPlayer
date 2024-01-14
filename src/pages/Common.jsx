eaeef99f595dbb8208d0d835ee7e6e3cd329a54c
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-13 22:46:39 -0300

    iniciando a renderização das musicas

--- src/pages/Common.jsx
+++ src/pages/Common.jsx
@@ -1,10 +1,14 @@
 import Navbar from '../components/Common/Navbar.jsx';
 import Menu from '../components/Global/Menu.jsx';
+import Title from '../components/Global/Title.jsx'
 
 function Common() {
   return (
     <div>
       <Navbar />
+      <div className='titleFavorites'>
+        <Title title="Favorites" />
+      </div>
       <Menu />
     </div>
   )
