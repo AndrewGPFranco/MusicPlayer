52b491564f918b50fe5a35bbd0b829ab2618e150
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-15 20:29:25 -0300

    pagina about v5

--- src/components/About/Information/Information.jsx
+++ src/components/About/Information/Information.jsx
@@ -1,5 +1,6 @@
 import clock from "../../../assets/time.png"
 import settings from "../../../assets/settings.png"
+import right from "../../../assets/right.png"
 import style from "./Information.module.css"
 
 export default function Information() {
@@ -12,6 +13,10 @@ export default function Information() {
             <div className={style.container}>
                 <img src={settings} alt="Settings" />
                 <p>Developer in Repl.it and Vs Code!</p>
+            </div>
+            <div className={style.container}>
+                <img src={right} alt="Right" />
+                <p>All Right Reserved to Andrew Silva!</p>
             </div>
         </div>
     )
