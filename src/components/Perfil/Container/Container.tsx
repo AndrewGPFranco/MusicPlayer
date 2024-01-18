5acacb583b69531e3a914c4bfb39b84b61f988a2
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-17 22:07:14 -0300

    pagina de perfil v2

--- src/components/Perfil/Container/Container.tsx
+++ src/components/Perfil/Container/Container.tsx
@@ -1,22 +1,28 @@
 import style from "./Container.module.css";
 import iconMusic from "../../../assets/iconMusic.png";
 import iconClock from "../../../assets/iconClock.png";
-import profile from '../../../assets/perfil.jpg'
+import profile from "../../../assets/perfil.jpg";
+import banner from '../../../assets/banner.jpg'
 
 export default function Container() {
   return (
     <section className={style.containerDad}>
-      <div className={style.containerOne}>
-        <img src={iconMusic} alt="Icone de Music" />
-        <p>10 Sounds</p>
+      <div className={style.container}>
+        <img src={banner} alt="Banner de uma imagem do React Js" />
       </div>
-      <div className={style.photo}>
-        <img src={profile} alt="Imagem de Perfil" />
-        <h1>Andrew</h1>
-      </div>
-      <div className={style.containerThree}>
-        <img src={iconClock} alt="Icone de Music" />
-        <p>10 Sounds</p>
+      <div className={style.profile}>
+        <div className={style.containerOne}>
+          <img src={iconMusic} alt="Icone de Music" />
+          <p>10 Sounds</p>
+        </div>
+        <div className={style.photo}>
+          <img src={profile} alt="Imagem de Perfil" />
+          <h1>Andrew</h1>
+        </div>
+        <div className={style.containerThree}>
+          <img src={iconClock} alt="Icone de Music" />
+          <p>5 minutes</p>
+        </div>
       </div>
     </section>
   );
--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,11 +1,9 @@
 import Menu from '../components/Global/Menu';
-import Banner from "../components/Perfil/Banner/Banner";
 import Container from '../components/Perfil/Container/Container';
 
 function Perfil() {
   return (
     <div className="containerPerfil">
-      <Banner />
       <Container />
       <Menu />
     </div>
